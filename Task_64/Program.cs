9caca56e8818a7df9f67a394920a32df8d6b425c
Ilippy <Ilippyk@gmail.com>
2023-01-30 16:01:38 +0300

    add task 64

---
+++ Task_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using NumberMain;
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = Numbers.EnterNumber("Введите целое число N");
+        Console.WriteLine($"N = {number} -> \"{recursive(number)}\"");
+    }
+
+    static string recursive(int number){
+        if(number == 1) return "1";
+        return $"{number}, " + recursive(number-1);
+    }
+}
